4dd65894c23c7ae2c4947bedcd7b2f8ef79042d0
MichaelOnyoin <61825881+MichaelOnyoin@users.noreply.github.com>
2024-05-27 09:47:19 +0000

    Changes to be committed: 	modified:   src/app/hero.tsx 	modified:   src/components/fixed-plugin.tsx 	modified:   src/components/footer.tsx

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -4,7 +4,7 @@ import { Button } from "@material-tailwind/react";
 
 export function FixedPlugin() {
   return (
-    <a href="https://www.material-tailwind.com" target="_blank">
+    <a href="https://nextjs.org" target="_blank">
       <Button
         color="white"
         size="sm"
@@ -17,7 +17,7 @@ export function FixedPlugin() {
           alt="Material Tailwind"
           src="https://www.material-tailwind.com/favicon.png"
         />{" "}
-        Made With Material Tailwind
+        Made With Next JS
       </Button>
     </a>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,15 +9,11 @@ export function Footer() {
       <div className="container mx-auto">
         <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
           <Typography className="text-center font-normal !text-gray-700">
-            &copy; {CURRENT_YEAR} Made with{" "}
+            &copy; {CURRENT_YEAR} Created by{" "}
             <a href="https://www.material-tailwind.com" target="_blank">
-              Material Tailwind
+              Michael Onyoin
             </a>{" "}
-            by{" "}
-            <a href="https://www.creative-tim.com" target="_blank">
-              Creative Tim
-            </a>
-            .
+            
           </Typography>
           <ul className="flex gap-8 items-center">
             {LINKS.map((link) => (
@@ -32,7 +28,7 @@ export function Footer() {
                 </Typography>
               </li>
             ))}
-            <Button color="gray">subscribe</Button>
+            <Button color="gray">Hire Me</Button>
           </ul>
         </div>
       </div>
